537541ecb7e8b3af1ba0b92571f48c0874c712d0
keerthidhanush <148602974+keerthidhanush@users.noreply.github.com>
2024-02-17 22:04:05 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "library_system";
+// <?php
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "library_system";
 
-// Create connection
-$database = new mysqli($servername, $username, $password, $dbname);
+// // Create connection
+// $database = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($database->connect_error) {
-    die("Connection failed: " . $database->connect_error);
-}
-?>
+// // Check connection
+// if ($database->connect_error) {
+//     die("Connection failed: " . $database->connect_error);
+// }
+// ?>
